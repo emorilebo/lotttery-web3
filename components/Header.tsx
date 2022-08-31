1b7e238222a66f6472f8c95343b358f33df9baf1
Godfrey Lebo <emorylebo@gmail.com>
2022-08-31 16:42:33 +0100

    UPDATE Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return <div>
+    <div>
+        <img src="/emori.jpeg" alt="" />
+    </div>
+  </div>;
 }
 
 export default Header;
