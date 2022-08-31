3dd7b9dbfc64bd191f9d149d83470fd15676b339
Godfrey Lebo <emorylebo@gmail.com>
2022-08-31 17:06:53 +0100

    UPDATE Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 function Header() {
-  return <div>
+  return (
     <div>
+      <div className="flex items-center space-x-2">
         <img className="h-20 w-20 rounded-full" src="/emori.jpeg" alt="emori" />
+        <div>
+          <h1 className="text-lg text-white font-bold">The Curiosity Draw</h1>
+          <p className="text-sm text-emerald-500 truncate">User...</p>
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default Header;
