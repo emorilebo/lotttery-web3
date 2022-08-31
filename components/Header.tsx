28a56268640519dbb375426114d8a86348d8b476
Godfrey Lebo <emorylebo@gmail.com>
2022-08-31 17:47:13 +0100

    UPDATE NavButton

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import NavButton from "./NavButton";
+import { Bars3BottomRightIcon } from "@heroicons/react/24/solid";
 
 function Header() {
   return (
-    <div>
+    <header className="grid grid-cols-3">
       <div className="flex items-center space-x-2">
         <img className="h-20 w-20 rounded-full" src="/emori.jpeg" alt="emori" />
         <div>
@@ -13,12 +14,16 @@ function Header() {
       </div>
 
       <div>
-        <div className="bg-[#0A1F1C]">
+        <div className="bg-[#0A1F1C] p-4 space-x-2">
           <NavButton isActive title="Buy Tickets" />
           <NavButton title="Logout" />
         </div>
       </div>
-    </div>
+
+      <div>
+        <Bars3BottomRightIcon className="h-8 w-8 mx-auto text-white cursor-pointer" />
+      </div>
+    </header>
   );
 }
 
