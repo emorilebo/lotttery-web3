9a4dd9dbce4cdac878ea46c56c3c151691a112fd
Godfrey Lebo <emorylebo@gmail.com>
2022-09-01 05:51:28 +0100

    UPDATE header

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,12 +8,12 @@ function Header() {
       <div className="flex items-center space-x-2">
         <img className="h-20 w-20 rounded-full" src="/emori.jpeg" alt="emori" />
         <div>
-          <h1 className="text-lg text-white font-bold">The Curiosity Draw</h1>
+          <h1 className="text-lg text-white font-bold">The Curiosity Lottery</h1>
           <p className="text-sm text-emerald-500 truncate">User...</p>
         </div>
       </div>
 
-      <div className="hidden  md:flex  items-center justify-center rounded-md md:col-span-3">
+      <div className=" hidden md:flex  items-center justify-center rounded-md md:col-span-3">
         <div className="bg-[#0A1F1C] p-4 space-x-2">
           <NavButton isActive title="Buy Tickets" />
           <NavButton title="Logout" />
